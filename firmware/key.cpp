62579bad0b136e630bf46c341937809ce16a7e42
System Administrator <root@10ddb19ed56f.ant.amazon.com>
2014-10-29 13:52:13 +0530

    Libs

--- firmware/key.cpp
+++ firmware/key.cpp
@@ -1,4 +1,4 @@
 #include "keys.h"
 
-const char* awsKeyID = "AKIAJQUGWOWPFITEOGTQ";
-const char* awsSecKey = "8ct1xeZXMJkM7DJNyiPxihaafRcHGh0/CJkpxBzw";
+const char* awsKeyID = "YOURKEYSHERE";
+const char* awsSecKey = "YOURSECRETHERE";
